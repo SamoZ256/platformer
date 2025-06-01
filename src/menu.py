b56a51d9c239971fc647453862f502e0f8c5cce6
Samuliak <samuliak77@gmail.com>
2025-06-01 15:47:25 +0200

    reorganize

--- src/menu.py
+++ src/menu.py
@@ -1,53 +1,48 @@
-from button import Button
 import pygame
 import sys
 
-from src.main import play_game
+from constants import *
+from game import play_game
+from button import Button
 
 
 class Menu:
-    def __init__(self, SCREEN_WIDTH, SCREEN_HEIGHT, BG_PATH):
-        self.SCREEN_WIDTH = SCREEN_WIDTH
-        self.SCREEN_HEIGHT = SCREEN_HEIGHT
+    def __init__(self, screen, BG_PATH):
         self.BG = pygame.image.load(BG_PATH)
-        pygame.display.set_caption("Menu")
-        self.SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.screen = screen
 
     def get_font(self, size):  # Returns the font in the desired size
-        return pygame.font.Font("Minecraft.ttf", size)
+        return pygame.font.Font("assets/Minecraft.ttf", size)
 
     def main_menu(self):
         while True:
-            self.SCREEN.blit(self.BG, (0, 0))
+            self.screen.blit(self.BG, (0, 0))
 
             MENU_MOUSE_POS = pygame.mouse.get_pos()
 
             MENU_TEXT = self.get_font(100).render("MAIN MENU", True, "WHITE")
-            MENU_RECT = MENU_TEXT.get_rect(center=(self.SCREEN_WIDTH // 2, 100))
+            MENU_RECT = MENU_TEXT.get_rect(center=(SCREEN_WIDTH // 2, 100))
 
 
-            PLAY_BUTTON = Button(image=pygame.image.load("11zon_resized(1).png"), pos=(self.SCREEN_WIDTH // 2, 300),
+            PLAY_BUTTON = Button(image=pygame.image.load("assets/11zon_resized(1).png"), pos=(SCREEN_WIDTH // 2, 300),
                                  text_input="PLAY", font=self.get_font(75), base_color="GREEN", hovering_color="WHITE",)
-            QUIT_BUTTON = Button(image=pygame.image.load("11zon_resized(1).png"), pos=(self.SCREEN_WIDTH // 2, 450),
+            QUIT_BUTTON = Button(image=pygame.image.load("assets/11zon_resized(1).png"), pos=(SCREEN_WIDTH // 2, 450),
                                  text_input="QUIT", font=self.get_font(75), base_color="GREEN", hovering_color="WHITE",)
 
-            self.SCREEN.blit(MENU_TEXT, MENU_RECT)
+            self.screen.blit(MENU_TEXT, MENU_RECT)
 
             for button in [PLAY_BUTTON, QUIT_BUTTON]:
                 button.changeColor(MENU_MOUSE_POS)
-                button.update(self.SCREEN)
+                button.update(self.screen)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+                    return
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
-                        from src.main import play_game  # Import here to avoid circular import!
-                        play_game()  # <-- THIS WILL SHOW THE GAME WORLD!
+                        play_game(self.screen)
+                        return
                     if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
-                        pygame.quit()
-                        sys.exit()
+                        return
 
             pygame.display.update()
-
--- src/run.py
+++
@@ -1,7 +0,0 @@
-from menu import Menu
-import pygame
-
-if __name__ == "__main__":
-    pygame.init()
-    menu = Menu(800, 600, "F_BG.png")
-    menu.main_menu()
